e1c7d826c176d80db75601e6f20f189bd32c9144
叶磊 <yelei@xmfunny.com>
2020-07-17 19:16:13 +0800

    cloth

---
+++ Cloth/Cloth.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct ClothVertex : IComponentData
+{
+    public NativeArray<int> pins;
+    public NativeArray<float3> vertices;
+    public NativeArray<float3> oldVertices;
+}
+
+public struct ClothBar : IComponentData
+{
+    public NativeArray<float> barLengths;
+    public NativeArray<int2> bars;
+}
+
+public struct ClothMesh : ISharedComponentData
+{
+    public Mesh mesh;
+}
---
+++ Cloth/InitializeSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Jobs;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public abstract class InitializeSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+
+    }
+
+    protected override void OnUpdate()
+    {
+
+    }
+}
